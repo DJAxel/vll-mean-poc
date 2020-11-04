d3a79793b660c1ea5b862d3087d3e1b1c74cc541
Axel Köhler <axel.kohler@outloo.com>
2020-11-04 16:06:03 +0100

    Retrieve the uploaded file in the backend and output it's properties in the terminal

--- NodeJS/controllers/dataController.js
+++ NodeJS/controllers/dataController.js
@@ -1,9 +1,21 @@
 const express = require('express');
-let router = express.Router();
-let ObjectId = require('mongoose').Types.ObjectId;
+const fileUpload = require('express-fileupload');
+const app = express();
 
-router.get('/upload', (req, res) => {
+app.use(fileUpload());
+
+app.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-module.exports = router;
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+    res.send('File uploaded!');
+});
+
+module.exports = app;
